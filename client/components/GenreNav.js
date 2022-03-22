b57fc795f0b471480c2e39e3dc51e7634ffb6611
Gal Gir <ggir@mail.yu.edu>
2022-03-22 15:25:11 -0400

    fantasy genre page fixed

--- client/components/GenreNav.js
+++ client/components/GenreNav.js
@@ -6,7 +6,7 @@ const GenreNav = () => (
   <div >
     <Navbar bg = "dark" className="justify-content-center" variant = "light" >
      <Nav.Link href = "/genre/Fiction" >Fiction</Nav.Link>
-     <Nav.Link href = "genre/Fantasy" >Fantasy</Nav.Link>
+     <Nav.Link href = "/genre/Fantasy" >Fantasy</Nav.Link>
      <Nav.Link href = "/genre/Romance" >Romance</Nav.Link>
      <Nav.Link href = "/genre/Classics" >Classics</Nav.Link>
      <Nav.Link href = "/genre/Childrens" >Childrens</Nav.Link>
