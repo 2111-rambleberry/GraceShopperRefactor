d279e2285787ed640ba0abe8956495e8230e8e2b
keranie theodosiou <keranie.theodosiou@gmail.com>
2022-05-12 22:31:01 -0400

    top nav

--- client/components/TopNav.js
+++ client/components/TopNav.js
@@ -37,8 +37,7 @@ const TopNav = () => {
     <div>
       {/* <Stack direction="horizontal" gap={2}> */}
       <Navbar collapseOnSelect sticky="top" expand="lg" bg="light">
-        {/* <Container> */}  
-        
+        {/* <Container> */}          
         <Navbar.Brand href="/" className="logo">
           <Stack direction="horizontal" gap={1} >
             <Image  src="../logo-updated.png" height = '100px'/>       
@@ -108,102 +107,4 @@ const TopNav = () => {
   );
 }
 
-
 export default TopNav;
-
-
-//This Navbar should have the login, Logo, dropdown for admin and cart icon
-// const TopNav = ({ handleClick, isLoggedIn, isUserAdmin, username, books }) => (
-//   <div>
-//     {/* <Stack direction="horizontal" gap={2}> */}
-//     <Navbar collapseOnSelect sticky="top" expand="lg" bg="light">
-//       {/* <Container> */}  
-      
-//       <Navbar.Brand href="/homepage" className="logo">
-//         <Stack direction="horizontal" gap={1} >
-//           {/* <Image  src = 'cassetteLogo.png' height = '70px'/>        */}
-//           {/* <img src = 'lampLogo.png' height = '70px' width = '70px'/>  */}
-//           {/* <img src = 'shelfLogo.png' height = '100px' width = '100px'/>  */}
-//           <h1 className="logo">BookShopper</h1>
-//         </Stack>
-//       </Navbar.Brand>
-
-//       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//       <Navbar.Collapse id="responsive-navbar-nav">
-//       <Nav className = "ms-auto"> 
-//         <Form className="d-flex">
-//           <FormControl
-//             type="search"
-//             placeholder="Search"
-//             className="me-2"
-//             aria-label="Search"
-//           />
-//         </Form>
-//         <Button variant="outline-primary">
-//           <BsSearch />
-//         </Button>
-//       </Nav> 
-//       <Nav className="ms-auto">
-//         {isLoggedIn ? (
-//           <>  
-//             <Nav.Link disabled>Hello {username}!</Nav.Link>         
-//             <Nav.Link href="/edit">Profile</Nav.Link>
-//             {/* The navbar will show these links after you log in */}
-//             <Nav.Link href="#" onClick={handleClick}>
-//               Logout
-//             </Nav.Link>
-//             {isUserAdmin && (
-//               <NavDropdown
-//                 title="Admin Portal"
-//                 id="collasible-nav-dropdown"
-//               >
-//                 <NavDropdown.Item href="/stock">Stock</NavDropdown.Item>
-//                 <NavDropdown.Item href="/users">Users</NavDropdown.Item>
-//                 <NavDropdown.Item href="/add-book">
-//                   Add Book
-//                 </NavDropdown.Item>
-//               </NavDropdown>
-//             )}
-//           </>
-//         ) : (
-//           <>
-//             {/* The navbar will show these links before you log in */}
-//             <Nav.Link href = '/login'>Login</Nav.Link>
-//           </>
-//         )}
-//       </Nav>
-//       <Nav onChange={() => handleLoad}>
-//         <Nav.Link href="/cart">
-//             <BsFillBasket3Fill/>
-//           </Nav.Link>
-//           <Badge pill className="cart-items">
-//             {!books ? 0 : books.length }
-//           </Badge>
-//         </Nav>
-//       </Navbar.Collapse>
-//       {/* </Container> */}
-//     </Navbar>
-//     {/* </Stack> */}
-//   </div>
-// );
-
-
-
-// const mapState = (state) => {
-//   return {
-//     isLoggedIn: !!state.auth.id,
-//     isUserAdmin: !!state.auth.isAdmin,
-//     username: state.auth.username,
-//     books: state.cartReducer.books
-//   };
-// };
-
-// const mapDispatch = (dispatch) => {
-//   return {
-//     handleClick() {
-//       dispatch(logout());
-//     },
-//   };
-// };
-
-// export default connect(mapState, mapDispatch)(TopNav);
